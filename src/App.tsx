3d436b497f81ff937ce7ba89723be5c5e8d691a1
Pankaj Kumar Pandit <143678006+panditpankaj21@users.noreply.github.com>
2024-10-17 18:56:10 +0530

    removed variable which never used

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import Sidebar from "./components/Sidebar"
 import MainContent from "./components/MainContent"
 import React, { Suspense } from "react"
 import { NavBar } from "./components/NavBar"
